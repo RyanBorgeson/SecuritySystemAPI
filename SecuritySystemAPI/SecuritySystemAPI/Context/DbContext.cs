58a00b174410943fc8dca9f8cea837ec626998e6
Ryan Borgeson <ryanborgeson@gmail.com>
2016-10-22 18:33:33 -0400

    Updated API Help page and configured code first migrations.

---
+++ SecuritySystemAPI/SecuritySystemAPI/Context/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SecuritySystemAPI.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        { }
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
